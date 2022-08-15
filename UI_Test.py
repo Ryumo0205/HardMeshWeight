63fb46bdf5f1f9c64899fa2895864b6cc9508b28
SigenChang <jian02051993@gmail.com>
2022-08-15 21:23:03 +0800

    取消ui的視窗布局,會造成無法呼叫對象

--- UI_Test.py
+++ UI_Test.py
@@ -1,15 +1,16 @@
 import pymel.core as pm
-import pymel.core.uitypes as pmui
-import pymel.core.windows as pmw
+
 
 ui_file_path = pm.internalVar(usd=True) + r"Test/HardMeshWeight_UI.ui"
 print(ui_file_path)
 MainUI = pm.loadUI(uiFile=ui_file_path)
 print(MainUI)
 
-x = pm.textField( MainUI + r"|lineEdit",query=True)
+#x = pm.text( MainUI + r"|State_label",edit=True)
+
 
 
 def fix_weight_cmd( ignoreInputs ):
     print("state_edit")
+    pm.text( MainUI + r"|State_label",edit=True,label="OK!")
 pm.showWindow( MainUI )
